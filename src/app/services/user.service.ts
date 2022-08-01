c6cd3bdde8ef9c10129d488a59be370f06eea17b
FionaWekulo <fionawekulo+1@gmail.com>
2022-08-01 18:44:35 +0300

    Set up apiUrl as an environment variable

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,22 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from 'app/interfaces/user';
+import { environment } from 'environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-
+  private apiUrl = environment.apiUrl;
   constructor(
     private http: HttpClient
   ) { }
 
   //Many users
   getUsers(): Observable<User[]>{
-    return this.http.get<User[]>('https://jsonplaceholder.typicode.com/users')
+    return this.http.get<User[]>(`${this.apiUrl}/users`)
   }
 
   //single user
   getUser(): Observable<User>{
-    return this.http.get<User>('https://jsonplaceholder.typicode.com/users/1')
+    return this.http.get<User>(`${this.apiUrl}/users/1`)
   }
 }
